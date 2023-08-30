efc86322b515145377872e3fd7d22776f55b77d9
Michael Darmanis <michaledarm@gmail.com>
2023-08-30 22:30:28 +0300

    Added preprocessing and some functionality.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import os
+import sys
 import fetch_data
+import process_data
 
 def create_raw_dataset():
     zip_url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.zip?1f54ac4889a7e6d01b17d729b1c02549"
     zip_path = "eurofxref-hist.zip"
     unzip_dir = "../dataset"
     original_name = "eurofxref-hist.csv"
-    new_name = "dataset.csv"
+    new_name = "raw_dataset.csv"
     
     # Create the directory if it does not exist
     if not os.path.exists(unzip_dir):
@@ -17,6 +19,19 @@ def create_raw_dataset():
         print("Downloaded, unzipped, renamed, and deleted the original forex-zip file successfully.")
     else:
         print("Failed to download the file.")
+        sys.exit(1)
+
+def process_dataset():
+    unzip_dir = "../dataset"
+    old_name = "raw_dataset.csv"
+    new_name = "processed_dataset.csv"
+    cleaned_csv_file_path = os.path.join(unzip_dir, new_name)
+
+    # Clean the data
+    process_data.clean_data(os.path.join(unzip_dir, old_name),
+                            cleaned_csv_file_path)
+        
 
 if __name__ == "__main__":
     create_raw_dataset()
+    process_dataset()
