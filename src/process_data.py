1f1cd2e3b00fb8820c109b09652f9267b6808819
Michael Darmanis <michaledarm@gmail.com>
2023-09-02 22:32:58 +0300

    Added resampling function.

--- src/process_data.py
+++ src/process_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def clean_data(csv_file_path, cleaned_csv_file_path):
@@ -8,13 +9,8 @@ def clean_data(csv_file_path, cleaned_csv_file_path):
     - csv_file_path (str): The path to the original CSV file.
     - cleaned_csv_file_path (str): The path to save the cleaned CSV file.
     """
-    # Read the entire CSV file
     df = pd.read_csv(csv_file_path)
-
-    # Convert the 'Date' column to a datetime type
     df['Date'] = pd.to_datetime(df['Date'])
-
-    # Sort the dataframe by date
     df = df.sort_values('Date')
 
     # Find columns that have NaN values
@@ -32,8 +28,40 @@ def clean_data(csv_file_path, cleaned_csv_file_path):
 
     # Trim the DataFrame to only include the common time period
     df = df[(df['Date'].dt.year >= min_year) & (df['Date'].dt.year <= max_year)]
-
-    # Save the cleaned data to a new CSV file
     df.to_csv(cleaned_csv_file_path, index=False)
 
     print(f"Data cleaned and saved to {cleaned_csv_file_path}. Dropped columns: {drop_columns}")
+
+
+def resample_data(input_csv_path, output_folder):
+    """
+    Resamples the dataset to different frequencies (daily, weekly, monthly, quarterly, yearly)
+    and saves them into separate CSV files.
+
+    Parameters:
+    - input_csv_path (str): The path to the original CSV file.
+    - output_folder (str): The folder where the resampled CSV files will be saved.
+    """
+    df = pd.read_csv(input_csv_path)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    
+    # Resampling frequencies
+    frequencies = {
+        'daily': 'D',
+        'weekly': 'W',
+        'monthly': 'M',
+        'quarterly': 'Q',
+        'yearly': 'Y'
+    }
+    
+    # Create the output folder if it does not exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    for freq_name, freq_code in frequencies.items():
+        resampled_df = df.resample(freq_code).mean()
+        resampled_df.dropna(how='all', inplace=True)
+        
+        output_csv_path = os.path.join(output_folder, f"{freq_name}.csv")
+        resampled_df.to_csv(output_csv_path)
