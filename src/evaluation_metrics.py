4efc09d23ee53bc14cb082c2c399cc821957ed64
Michael Darmanis <michaledarm@gmail.com>
2023-09-09 03:02:45 +0300

    Torch version of Slawek's loss function.

--- src/evaluation_metrics.py
+++ src/evaluation_metrics.py
@@ -1,74 +1,28 @@
-import numpy as np
+import torch
+import torch.nn.functional as F
 
 
-def mse(y, y_hat):
+def sMAPE(y_pred, y_true):
     """
-    Calculates Mean Squared Error.
-
-    Parameters:
-    - y (numpy array): Actual test values.
-    - y_hat (numpy array): Predicted values.
-
-    Returns:
-    - float: Mean Squared Error.
+    Compute Symmetric Mean Absolute Percentage Error (sMAPE).
     """
-    y = np.reshape(y, (-1,))
-    y_hat = np.reshape(y_hat, (-1,))
-    mse = np.mean(np.square(y - y_hat)).item()
-    return mse
+    num = torch.abs(y_pred - y_true)
+    denom = (torch.abs(y_pred) + torch.abs(y_true)) / 2
+    smape_val = num / (denom + 1e-10)  # added epsilon to avoid division by zero
+    return 2 * torch.mean(smape_val)
 
-
-def mape(y, y_hat):
+def MASE(training_series, y_pred, y_true):
     """
-    Calculates Mean Absolute Percentage Error.
-
-    Parameters:
-    - y (numpy array): Actual test values.
-    - y_hat (numpy array): Predicted values.
-
-    Returns:
-    - float: Mean Absolute Percentage Error.
+    Compute Mean Absolute Scaled Error (MASE).
     """
-    y = np.reshape(y, (-1,))
-    y_hat = np.reshape(y_hat, (-1,))
-    mape = np.mean(np.abs(y - y_hat) / np.abs(y))
-    return mape
+    n = training_series.shape[0]
+    d = torch.abs(y_true - y_pred)
+    scale = torch.mean(torch.abs(training_series[1:n] - training_series[0:n-1])) + 1e-10  # added epsilon to avoid division by zero
+    mase_val = d / scale
+    return torch.mean(mase_val)
 
-
-def smape(y, y_hat):
+def hybrid_loss(training_series, y_pred, y_true):
     """
-    Calculates Symmetric Mean Absolute Percentage Error.
-
-    Parameters:
-    - y (numpy array): Actual test values.
-    - y_hat (numpy array): Predicted values.
-
-    Returns:
-    - float: Symmetric Mean Absolute Percentage Error.
+    Hybrid loss function combining sMAPE and MASE.
     """
-    y = np.reshape(y, (-1,))
-    y_hat = np.reshape(y_hat, (-1,))
-    smape = np.mean(2.0 * np.abs(y - y_hat) / (np.abs(y) + np.abs(y_hat)))
-    return smape
-
-
-def mase(y, y_hat, y_train, seasonality):
-    """
-    Calculates Mean Absolute Scaled Error.
-
-    Parameters:
-    - y (numpy array): Actual test values.
-    - y_hat (numpy array): Predicted values.
-    - y_train (numpy array): Actual train values for Naive1 predictions.
-    - seasonality (int): Main frequency of the time series (Quarterly 4, Daily 7, Monthly 12).
-
-    Returns:
-    - float: Mean Absolute Scaled Error.
-    """
-    y_hat_naive = []
-    for i in range(seasonality, len(y_train)):
-        y_hat_naive.append(y_train[(i - seasonality)])
-
-    masep = np.mean(abs(y_train[seasonality:] - y_hat_naive))
-    mase = np.mean(abs(y - y_hat)) / masep
-    return mase
+    return 0.5 * sMAPE(y_pred, y_true) + 0.5 * MASE(training_series, y_pred, y_true)
