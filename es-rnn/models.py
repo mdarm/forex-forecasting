c9e4fb58909fd0b7280a6191e598530df863e48d
Michael Darmanis <michaledarm@gmail.com>
2023-09-10 17:48:13 +0300

    Added optional trend use in the ESRNN class.

--- es-rnn/models.py
+++ es-rnn/models.py
@@ -3,18 +3,20 @@ import torch
 import torch.nn as nn
 
 
-class HoltWintersNoTrend(nn.Module):
+class HoltWinters(nn.Module):
     
-    def __init__(self, init_a=0.1, init_g=0.1, slen=12):
-        super(HoltWintersNoTrend, self).__init__()
+    def __init__(self, init_a=0.1, init_b=0.1, init_g=0.1, slen=12):
+        super(HoltWinters, self).__init__()
         
         # Smoothing parameters
         self.alpha = nn.Parameter(torch.tensor(init_a))
+        self.beta = nn.Parameter(torch.tensor(init_b))  # Trend smoothing parameter
         self.gamma = nn.Parameter(torch.tensor(init_g))
         
         # Initial parameters
         self.init_season = nn.Parameter(torch.tensor(np.random.random(size=slen)))
-        
+        self.init_trend = nn.Parameter(torch.tensor(0.1))        
+
         # Season length used to pick appropriate past season step
         self.slen = slen
         
@@ -32,53 +34,67 @@ class HoltWintersNoTrend(nn.Module):
         seasonals = [x.squeeze() for x in torch.split(seasonals, 1, dim=1)]
         
         result = []
+        trend = self.init_trend.repeat(batch_size)  # Initialize trend for each batch
         
         if rv:
             value_list = []
             season_list = []
+            trend_list = []
         
         for i in range(series.shape[1] + n_preds):
             if i == 0:
                 smooth = series[:, 0]
-                result.append(smooth)
+                result.append(smooth + trend)
                 if rv:
                     value_list.append(smooth)
                     season_list.append(seasonals[i % self.slen])
+                    trend_list.append(trend)
             else:
                 smooth_prev = smooth
+                trend_prev = trend
                 season_prev = seasonals[i % self.slen]
-                smooth = self.alpha * (series[:, i] - season_prev) + (1 - self.alpha) * smooth_prev
+                
+                # Update equations
+                smooth = self.alpha * (series[:, i] - season_prev) + (1 - self.alpha) * (smooth_prev + trend_prev)
+                trend = self.beta * (smooth - smooth_prev) + (1 - self.beta) * trend_prev
                 seasonals.append(self.gamma * (series[:, i] - smooth) + (1 - self.gamma) * season_prev)
                 
-                result.append(smooth + seasonals[i % self.slen])
+                result.append(smooth + trend + seasonals[i % self.slen])
                 
                 if rv:
                     value_list.append(smooth)
                     season_list.append(seasonals[i % self.slen])
+                    trend_list.append(trend)
         
         if rv:
-            return torch.stack(result, dim=1), torch.stack(value_list, dim=1), torch.stack(season_list, dim=1)
+            return torch.stack(result, dim=1), torch.stack(value_list, dim=1), torch.stack(season_list, dim=1), torch.stack(trend_list, dim=1)
         else:
             return torch.stack(result, dim=1)[:, -n_preds:]
 
 
 class ESRNN(nn.Module):
     
-    def __init__(self, hidden_size=16, slen=12, pred_len=12):
+    def __init__(self, hidden_size=16, slen=12, pred_len=12, use_trend=True):
         super(ESRNN, self).__init__()
         
-        self.hw = HoltWintersNoTrend(init_a=0.1, init_g=0.1)
+        self.hw = HoltWinters(init_a=0.1, init_b=0.1, init_g=0.1)
         self.rnn = nn.GRU(hidden_size=hidden_size, input_size=1, batch_first=True)
         self.lin = nn.Linear(hidden_size, pred_len)
         self.pred_len = pred_len
         self.slen = slen
+        self.use_trend = use_trend  # Whether to use the trend component or not
         
     def forward(self, series, shifts):
         batch_size = series.shape[0]
-        result, smoothed_value, smoothed_season = self.hw(series, shifts, rv=True, n_preds=0)
+        result, smoothed_value, smoothed_season, smoothed_trend = self.hw(series, shifts, rv=True, n_preds=0)
         
+        # De-seasonalize and de-level considering the trend if use_trend is True
         de_season = series - smoothed_season
-        de_level = de_season - smoothed_value
+        if self.use_trend:
+            de_level = de_season - smoothed_value - smoothed_trend
+        else:
+            de_level = de_season - smoothed_value
+        
         noise = torch.randn(de_level.shape[0], de_level.shape[1])
         noisy = de_level  # +noise
         noisy = noisy.unsqueeze(2)
@@ -89,4 +105,10 @@ class ESRNN(nn.Module):
         season_forecast = [smoothed_season[:, i % self.slen] for i in range(self.pred_len)]
         season_forecast = torch.stack(season_forecast, dim=1)
         
-        return smoothed_value[:, -1].unsqueeze(1) + season_forecast + pred
+        # Add back the trend for forecasting if use_trend is True
+        if self.use_trend:
+            trend_forecast = [smoothed_trend[:, i] for i in range(self.pred_len)]
+            trend_forecast = torch.stack(trend_forecast, dim=1)
+            return smoothed_value[:, -1].unsqueeze(1) + season_forecast + trend_forecast + pred
+        else:
+            return smoothed_value[:, -1].unsqueeze(1) + season_forecast + pred
