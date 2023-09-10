b6e56bb2a9160559769a63a164d63194f39379de
Michael Darmanis <michaledarm@gmail.com>
2023-09-10 18:14:18 +0300

    Organised code.

--- es-rnn/evaluation_metrics.py
+++ es-rnn/evaluation_metrics.py
@@ -5,15 +5,31 @@ import torch.nn.functional as F
 def sMAPE(y_pred, y_true):
     """
     Compute Symmetric Mean Absolute Percentage Error (sMAPE).
+
+    Parameters:
+    - y_pred (torch.Tensor): The predicted values.
+    - y_true (torch.Tensor): The true values.
+
+    Returns:
+    - torch.Tensor: The sMAPE loss value.
     """
     num = torch.abs(y_pred - y_true)
     denom = (torch.abs(y_pred) + torch.abs(y_true)) / 2
     smape_val = num / (denom + 1e-10)  # added epsilon to avoid division by zero
     return 2 * torch.mean(smape_val)
 
+
 def MASE(training_series, y_pred, y_true):
     """
     Compute Mean Absolute Scaled Error (MASE).
+
+    Parameters:
+    - training_series (torch.Tensor): The training time series data.
+    - y_pred (torch.Tensor): The predicted values.
+    - y_true (torch.Tensor): The true values.
+
+    Returns:
+    - torch.Tensor: The MASE loss value.
     """
     n = training_series.shape[0]
     d = torch.abs(y_true - y_pred)
@@ -21,8 +37,17 @@ def MASE(training_series, y_pred, y_true):
     mase_val = d / scale
     return torch.mean(mase_val)
 
+
 def hybrid_loss(training_series, y_pred, y_true):
     """
     Hybrid loss function combining sMAPE and MASE.
+
+    Parameters:
+    - training_series (torch.Tensor): The training time series data.
+    - y_pred (torch.Tensor): The predicted values.
+    - y_true (torch.Tensor): The true values.
+
+    Returns:
+    - torch.Tensor: The hybrid loss value.
     """
     return 0.5 * sMAPE(y_pred, y_true) + 0.5 * MASE(training_series, y_pred, y_true)
