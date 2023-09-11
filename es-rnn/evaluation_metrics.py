3184b399bdc80a2438bf9396a03795eb86e8d776
Michael Darmanis <michaledarm@gmail.com>
2023-09-11 13:30:02 +0300

    Added Mean Scaled Interval Score (MSIS) loss.

--- es-rnn/evaluation_metrics.py
+++ es-rnn/evaluation_metrics.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 
 
-def sMAPE(y_pred, y_true):
+def smape(y_pred, y_true):
     """
     Compute Symmetric Mean Absolute Percentage Error (sMAPE).
 
@@ -15,11 +15,12 @@ def sMAPE(y_pred, y_true):
     """
     num = torch.abs(y_pred - y_true)
     denom = (torch.abs(y_pred) + torch.abs(y_true)) / 2
-    smape_val = num / (denom + 1e-10)  # added epsilon to avoid division by zero
+    smape_val = num / (denom + 1e-10)
+
     return 2 * torch.mean(smape_val)
 
 
-def MASE(training_series, y_pred, y_true):
+def mase(training_series, y_pred, y_true):
     """
     Compute Mean Absolute Scaled Error (MASE).
 
@@ -33,8 +34,10 @@ def MASE(training_series, y_pred, y_true):
     """
     n = training_series.shape[0]
     d = torch.abs(y_true - y_pred)
-    scale = torch.mean(torch.abs(training_series[1:n] - training_series[0:n-1])) + 1e-10  # added epsilon to avoid division by zero
+
+    scale = torch.mean(torch.abs(training_series[1:n] - training_series[0:n-1])) + 1e-10
     mase_val = d / scale
+
     return torch.mean(mase_val)
 
 
@@ -50,4 +53,44 @@ def hybrid_loss(training_series, y_pred, y_true):
     Returns:
     - torch.Tensor: The hybrid loss value.
     """
-    return 0.5 * sMAPE(y_pred, y_true) + 0.5 * MASE(training_series, y_pred, y_true)
+    return 0.5 * smape(y_pred, y_true) + 0.5 * mase(training_series, y_pred, y_true)
+
+
+def compute_alpha(training_series):
+    """
+    Compute the alpha scaling factor using the simple naive method.
+    
+    Parameters:
+    - training_series (torch.Tensor): The training time series data.
+    
+    Returns:
+    - torch.Tensor: The computed scaling factor for the MSIS loss.
+    """
+    naive_forecast = torch.roll(training_series, shifts=1)
+    errors = training_series[1:] - naive_forecast[1:]
+    
+    alpha = torch.mean(torch.abs(errors))
+    
+    return alpha
+
+
+def msis(L, U, y_true, alpha):
+    """
+    Compute the Mean Scaled Interval Score (MSIS) loss.
+    
+    Parameters:
+    - L (torch.Tensor): Lower bound predictions.
+    - U (torch.Tensor): Upper bound predictions.
+    - y_true (torch.Tensor): Actual values.
+    - alpha (torch.Tensor): Scaling factor derived from in-sample one-step-ahead forecast errors.
+    
+    Returns:
+    - torch.Tensor: The MSIS loss value.
+    """
+    term1 = (U - L) / alpha
+    term2 = 2.0 / alpha * (L - y_true) * (y_true < L).float()
+    term3 = 2.0 / alpha * (y_true - U) * (y_true > U).float()
+
+    loss = term1 + term2 + term3
+    
+    return loss.sum()
