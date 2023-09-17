17eeb7a206bf6410fd47ce32155baac2dd0d183d
Michael Darmanis <michaledarm@gmail.com>
2023-09-17 20:02:47 +0300

    Added Class for early stopping.

--- es-rnn/utils.py
+++ es-rnn/utils.py
@@ -75,3 +75,21 @@ class SequenceLabeling(Dataset):
         shift_steps=start_int%self.seasonality
         
         return inp, out,shift_steps
+
+
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
